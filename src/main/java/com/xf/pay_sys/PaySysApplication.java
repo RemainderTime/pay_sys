d535e6eeadee2ec3bad14ce5c0058f1280055aa9
cd_xiongfeng <2439534736@qq.com>
2020-12-10 18:00:03 +0800

    初始化项目

---
+++ src/main/java/com/xf/pay_sys/PaySysApplication.java
@@ -0,0 +1,13 @@
+package com.xf.pay_sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaySysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaySysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xf/pay_sys/PaySysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xf.pay_sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaySysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
